4eb3eb71fb43d9efa625c43a12063f123de7a9e6
HederAb-ctrlc <cavalosbureos@gmail.com>
2025-07-31 20:11:04 -0600

    Se sube los cambios del proyecto

--- src/mx/com/ubam/serv/clases301m/Cliente.java
+++ src/mx/com/ubam/serv/clases301m/Cliente.java
@@ -1,25 +1,37 @@
 package mx.com.ubam.serv.clases301m;
 import java.io.*;
+
 import java.net.*;
 import java.util.*;
 
 public class Cliente {
-	Scanner sc = new Scanner(System.in);
+	public Cliente(){
 	try {
-		
-		ServerSocket ss= new ServerSocket (5050);
+		Scanner sc = new Scanner(System.in);
+		ServerSocket ss= new ServerSocket (5000);
 		Socket miSocket = new Socket("localhost",5001);
 		
-		System.out.println("Ingrese los numero que desea operar");}
+		System.out.println("Ingrese los numero que desea operar");
 	    String miAray = sc.next();
-	    InputStream is =miSocket.getOutputStream();
-	    OutputStream os = miSocket2.getInputStream();
-	    DataOutputStream dos = DataOutputStream(os);
-	    dos.writeUT(miArray);
+	    OutputStream os = miSocket.getOutputStream();
+	    DataOutputStream dos =new DataOutputStream(os);
+	    dos.writeUTF(miAray);
+	   
+	    Socket miSocket2= ss.accept();
+	    InputStream is = miSocket2.getInputStream();
+	    DataInputStream dis = new DataInputStream(is);
+	    System.out.println("Los numeros recibidos"+ dis.readUTF());
+	    miSocket.close();
+	    dis.close();
+	    miSocket2.close();
 	    
+  
 		
 		
-	}catch() {}
-	
-
+	}catch(Exception e) {System.err.print("Error de conexion");}
 }
+	public static void  main(String args[]) {
+		new Cliente();
+	}
+}
+
--- src/mx/com/ubam/serv/clases301m/Servidor.java
+++ src/mx/com/ubam/serv/clases301m/Servidor.java
@@ -5,15 +5,71 @@ import java.util.*;
 
 
 public class Servidor {
+	public Servidor() {
+	
 try {
+	Scanner sc= new Scanner(System.in);
 	ServerSocket ss = new ServerSocket(5001);
 	Socket miSocket = ss.accept();
 	InputStream is= miSocket.getInputStream();
 	DataInputStream dis= new DataInputStream(is);
-	String a = dis .readUTF();
-	String b= dis.readUTF();
-	System.out.println("Numeros recibidos: "+a);
+	String arraysev = dis .readUTF();
+	System.out.println("Numeros recibidos: "+arraysev);
+	int menu;
+	System.out.println("Presiona 1 si lo que quieres es sumar");
+	menu=sc.nextInt();
+	System.out.println("Presiona 2 si lo que quieres es el seno");
+	menu=sc.nextInt();
+	System.out.println("Presiona 3 si lo que quieres es el coseno");
+	menu=sc.nextInt();
+	System.out.println("Presiona 1 si lo que quieres es la raiz cuadrada");
+	menu=sc.nextInt();
+	System.out.println("Presiona 1 si lo que quieres es elevar a potencia");
+	menu=sc.nextInt();
+	
+	switch(menu) {
+	case 1: 
+		for(int i=0;1<arraysev.length();i++) {
+		int sumas=Integer.parseInt(arraysev)+Integer.parseInt(arraysev);
+		}
+	case 2:
+		for(int i=0;1<arraysev.length();i++) {
+		double conv=Double.parseDouble(arraysev);
+		double seno=Math.sin(conv);
+		}
+	case 3:
+		for(int i=0;1<arraysev.length();i++) {
+			double conv=Double.parseDouble(arraysev);
+			double seno=Math.cos(conv);
+			}
+	case 4:
+		for(int i=0;1<arraysev.length();i++) {
+			double conv=Double.parseDouble(arraysev);
+			double raiz=Math.sqrt(conv);
+		}
+	case 5:
+		int potencia;
+		System.out.println("Ingrese la potencia a a la que desea elevar:");
+		potencia=sc.nextInt();
+		for(int i=0;1<arraysev.length();i++) {
+			double conv=Double.parseDouble(arraysev);
+			double exp=Math.pow(conv,potencia);
+		}
+		
 	
 	
+	Socket miSocket2= new Socket("localhost",5000);
+	OutputStream os= miSocket2.getOutputStream();
+	DataOutputStream dos = new DataOutputStream(os);
+	dos.writeUTF(""+ menu);
+	miSocket.close();
+	dos.close();
+	miSocket2.close();
+	dis.close();
+	}
+}catch(Exception e) {System.err.print("Error dee conexion");}
+}
+public static void main(String args[]) {
+	new Servidor();
 }
 }
